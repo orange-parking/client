f0cd8c9624a5d80e2b80c05f0f80c21255664cf4
qoyyima22 <qoyyimafiassalam@gmail.com>
2020-11-06 01:44:07 +0700

    client

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,40 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from  'react-router-dom'
-import logo from '../logo.svg';
-import QRCode from 'qrcode'
-import db from '../firebase/index'
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import QRCode from "qrcode";
+import db from "../firebase/index";
 
 export default function Home() {
-    const [data, setData] = useState(null)
-    
-    useEffect(() => {
-        db.collection("parkings")
-        .onSnapshot((onSnapshot) => {
-            setData(onSnapshot)
-        })
-    },[])
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        let qr = QRCode.create('http://192.168.0.110:3000/create/'+JSON.stringify(new Date()).slice(1,-2))
-        QRCode.toDataURL(qr.segments, {color: {dark:'#000', light:'#0000'}},function (err, url) {
-            if (err) throw err
-            var img = document.getElementById('img')
-            img.src = url
-            console.log(url)
-        })
-    }, [data])
+  useEffect(() => {
+    db.collection("parkings").onSnapshot((onSnapshot) => {
+      setData(onSnapshot);
+    });
+  }, []);
 
-    return (
-        <div>
-            <header className="App-header">
-              <Link to={'/create/'+JSON.stringify(new Date()).slice(1,-2)}>
-                <h1>Parkir</h1>
-              </Link>
-              {/* <h1>Parkir</h1> */}
-              <img style={{width:'200px', height:'200px'}} src={logo} className="App-logo" alt="logo" />
-              <img style={{width:'500px', height:'500px'}} alt='qrcode' id="img" />
-            </header>
-        </div>
-    )
+  useEffect(() => {
+    let qr = QRCode.create(
+      "https://dev.foreach.id/create/" + JSON.stringify(new Date()).slice(1, -2)
+    );
+    QRCode.toDataURL(
+      qr.segments,
+      { color: { dark: "#000", light: "#0000" } },
+      function (err, url) {
+        if (err) throw err;
+        var img = document.getElementById("img");
+        img.src = url;
+        console.log(url);
+      }
+    );
+  }, [data]);
+
+  return (
+    <div>
+      <header className="App-header">
+        <Link to={"/create/" + JSON.stringify(new Date()).slice(1, -2)}>
+          <h1>Parkir</h1>
+        </Link>
+        {/* <h1>Parkir</h1> */}
+        <img
+          style={{ width: "200px", height: "200px" }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <img
+          style={{ width: "500px", height: "500px" }}
+          alt="qrcode"
+          id="img"
+        />
+      </header>
+    </div>
+  );
 }
